6836a2123f2bcc0237c41385a33a959daf4496be
Ludovic DEHON <tchiot.ludo@gmail.com>
2024-01-17 08:40:41 +0100

    feat: add a generic tap & target

---
+++ src/main/java/io/kestra/plugin/singer/taps/GenericTap.java
@@ -0,0 +1,77 @@
+package io.kestra.plugin.singer.taps;
+
+import io.kestra.core.exceptions.IllegalVariableEvaluationException;
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.models.tasks.RunnableTask;
+import io.kestra.core.runners.RunContext;
+import io.kestra.plugin.singer.models.Feature;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import javax.validation.constraints.NotNull;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "A Generic Singer tap."
+)
+public class GenericTap extends AbstractPythonTap implements RunnableTask<AbstractPythonTap.Output> {
+    @NotNull
+    @Schema(
+        title = "The list of pip package to install."
+    )
+    @PluginProperty
+    private List<String> pipPackages;
+
+    @NotNull
+    @Schema(
+        title = "The command to start."
+    )
+    @PluginProperty
+    private String command;
+
+    @NotNull
+    @Schema(
+        title = "The list of feature the connector supports."
+    )
+    @Builder.Default
+    private List<Feature> features = Arrays.asList(
+        Feature.PROPERTIES,
+        Feature.DISCOVER,
+        Feature.STATE
+    );
+
+    @NotNull
+    @Schema(
+        title = "The configuration to use",
+        description = "Will be save on config.json and used as arguments"
+    )
+    @PluginProperty(dynamic = true)
+    private Map<String, Object> configs;
+
+    public List<Feature> features() {
+        return this.features;
+    }
+
+    @Override
+    public Map<String, Object> configuration(RunContext runContext) throws IllegalVariableEvaluationException {
+        return runContext.render(configs);
+    }
+
+    @Override
+    public List<String> pipPackages() {
+        return this.pipPackages;
+    }
+
+    @Override
+    protected String command() {
+        return this.command;
+    }
+}
---
+++ src/main/java/io/kestra/plugin/singer/targets/GenericTarget.java
@@ -0,0 +1,69 @@
+package io.kestra.plugin.singer.targets;
+
+import io.kestra.core.exceptions.IllegalVariableEvaluationException;
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.models.tasks.RunnableTask;
+import io.kestra.core.runners.RunContext;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import javax.validation.constraints.NotNull;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "A Generic Singer target."
+)
+public class GenericTarget extends AbstractPythonTarget implements RunnableTask<AbstractPythonTarget.Output> {
+    @NotNull
+    @Schema(
+        title = "The list of pip package to install."
+    )
+    @PluginProperty
+    private List<String> pipPackages;
+
+    @NotNull
+    @Schema(
+        title = "The command to start."
+    )
+    @PluginProperty
+    private String command;
+
+    @NotNull
+    @Schema(
+        title = "The configuration to use",
+        description = "Will be save on config.json and used as arguments"
+    )
+    @PluginProperty(dynamic = true)
+    private Map<String, Object> configs;
+
+    @Override
+    public Map<String, Object> configuration(RunContext runContext) throws IllegalVariableEvaluationException, IOException {
+        return runContext.render(this.configs);
+    }
+
+    @Override
+    public List<String> pipPackages() {
+        return this.pipPackages;
+    }
+
+    @Override
+    protected String command() {
+        return this.command;
+    }
+
+    @Override
+    public Output run(RunContext runContext) throws Exception {
+        return super.runTarget(runContext);
+    }
+}
