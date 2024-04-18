610eaa68e86bc68eedcb0cabcbbbb1a1a64b21e9
Loïc Mathieu <loikeseke@gmail.com>
2024-04-18 16:59:07 +0200

    feat: change plugin category

--- src/main/java/io/kestra/plugin/singer/targets/package-info.java
+++ src/main/java/io/kestra/plugin/singer/targets/package-info.java
@@ -1,7 +1,7 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using Singer targets.\n" +
         "Singer is a Simple, Composable, Open Source ETL.",
-    categories = PluginSubGroup.PluginCategory.MISC //FIXME TOOL
+    categories = PluginSubGroup.PluginCategory.INGESTION
 )
 package io.kestra.plugin.singer.targets;
 
