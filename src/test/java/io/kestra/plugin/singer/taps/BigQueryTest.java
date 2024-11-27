1dd8ae3e08fb71ce2cc72fbf858fcae822ab7da8
Mathieu Gabelle <mgabelle@kestra.io>
2024-11-27 10:21:50 +0100

    fix(tests): remove limit for BigQuery test

--- src/test/java/io/kestra/plugin/singer/taps/BigQueryTest.java
+++ src/test/java/io/kestra/plugin/singer/taps/BigQueryTest.java
@@ -7,6 +7,7 @@ import io.kestra.core.runners.RunContextFactory;
 import io.kestra.core.utils.IdUtils;
 import io.kestra.core.utils.TestsUtils;
 import io.kestra.plugin.core.runner.Process;
+import io.kestra.plugin.scripts.runner.docker.Docker;
 import io.kestra.plugin.singer.models.DiscoverMetadata;
 import io.kestra.plugin.singer.models.StreamsConfiguration;
 import jakarta.inject.Inject;
@@ -37,7 +38,6 @@ class BigQueryTest extends TapsTest {
             .serviceAccount(serviceAccount)
             .startAlwaysInclusive(false)
             .startDateTime(Instant.parse("2013-09-08T16:19:12Z"))
-            .limit(1)
             .streams(Collections.singletonList(
                 BigQuery.Stream.builder()
                     .name("covid19_nyt_us_states")
